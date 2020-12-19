f9deaa2c7eafe5af4ca07c9485c41fc688fcc2ed
Dinara227 <32163613+Dinara227@users.noreply.github.com>
2020-12-19 21:03:35 +0200

    added models

---
+++ Schedule/Models/Group.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule.Models
+{
+    public class Group : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int Number { get; set; }
+        public Faculty Faculty { get; set; }
+    }
+}
---
+++ Schedule/Models/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule.Models
+{
+    public class Teacher : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Reference { get; set; }
+        public Faculty Faculty { get; set; }
+    }
+}
