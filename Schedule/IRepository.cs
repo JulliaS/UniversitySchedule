13865a2437ab0a47a477056b303bf57f01771e90
Yuliia Stakhiv <Yuliia_Stakhiv@epam.com>
2020-12-19 21:11:54 +0200

    Added repo

---
+++ Schedule/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using Schedule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Schedule
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task<T> Insert(T entity);
+        Task<T> Update(int id, T entity);
+        Task<bool> Delete(int id);
+        Task<IEnumerable<T>> GetWithInclude(params string[] includeProperties);
+        Task<T> GetWithIncludeById(int id, params string[] includeProperties);
+        Task<IEnumerable<T>> FindByCondition(Expression<Func<T, bool>> expression);
+    }
+}
