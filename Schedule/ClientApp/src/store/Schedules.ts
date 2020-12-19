a67e8c09c61ce76e295e03d390e028bdbe38def4
VITALII NASTOSIAK <v.nastosiak@gmail.com>
2020-12-19 21:49:13 +0200

    added Schedule model

---
+++ Schedule/ClientApp/src/store/Schedules.ts
@@ -0,0 +1,19 @@
+﻿import { Room } from "./rooms";
+import { Subject } from "./Subjects";
+import { Teacher } from "./Teachers";
+
+export interface Schedule{
+    id:number
+    numberDay: number
+    lesson:Lesson
+    teacher: Teacher
+    subject: Subject
+    room: Room
+}
+
+export interface Lesson{
+    id:number
+    number:number
+    startDate: Date
+    endDate: Date
+}
