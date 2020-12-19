04988a47f00b3751e22f0a532e800afa3568ecbc
Mirroslava <mirosya.barabash@gmail.com>
2020-12-19 21:15:05 +0200

    add ScheduleContext

---
+++ Schedule/ScheduleContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Schedule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule
+{
+    public class ScheduleContext : DbContext
+    {
+        public DbSet<Models.Schedule> Schedule{ get; set; }
+        public DbSet<Faculty> Faculties{ get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Availability> Availability { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Semester> Semesters { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+
+        public ScheduleContext(DbContextOptions<ScheduleContext> options) : base (options)
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+    }
+}
