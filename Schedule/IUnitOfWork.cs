be2038513674de8c8e37fe160dfa41be904c6f98
Yuliia Stakhiv <Yuliia_Stakhiv@epam.com>
2020-12-19 21:12:35 +0200

    Added Unit of work

---
+++ Schedule/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Schedule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule
+{
+    public interface IUnitOfWork<T> where T : BaseEntity
+    {
+        IRepository<T> Repository { get; }
+        void Save();
+    }
+
+}
---
+++ Schedule/UnitOfWork.cs
@@ -0,0 +1,49 @@
+﻿using Schedule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule
+{
+    public class UnitOfWork<T> : IUnitOfWork<T>, IDisposable where T : BaseEntity
+    {
+        private readonly ScheduleContext _context;
+        private IRepository<T> _repository;
+
+        public UnitOfWork(ScheduleContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<T> Repository
+        {
+            get { return _repository ?? (_repository = new Repository<T>(_context)); }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
